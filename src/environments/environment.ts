39f89c572022756c0ab2a2d311843797e02c2e50
Rajesh Pelne <rajesh.pelne90@gmail.com>
2021-08-05 16:31:50 +0530

    Update code

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,10 +4,10 @@
 declare const require: any;
 export const environment = {
   production: true,
-  api_endpoint: 'http://13.235.198.229:8080/',
+  api_endpoint: 'http://65.2.162.230:8080/',
   restapi_endpoint: 'http://rest-admin@test:103.233.25.218:8080/',
   version: require('../../package.json').version,
-  appBaseUrl: 'http://localhost:4200'
+  appBaseUrl: 'http://65.2.162.230:8080/dbox'
 };
 
 /*
